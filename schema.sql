53fd1c9d163b9fd75fbffba60d5e4db6d6994784
Vishva Kumara <vishva8kumara@gmail.com>
2025-11-16 12:05:53 +0530

    chore: database indexes for faster querying

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 
-CREATE SCHEMA IF NOT EXISTS raw;
-CREATE SCHEMA IF NOT EXISTS analytics;
+CREATE DATABASE raw;
 
 -- Raw server metrics table
 CREATE TABLE IF NOT EXISTS raw.metrics (
@@ -10,6 +9,12 @@ CREATE TABLE IF NOT EXISTS raw.metrics (
     metric_value DOUBLE PRECISION NOT NULL,
     ts TIMESTAMPTZ NOT NULL
 );
+CREATE INDEX IF NOT EXISTS idx_raw_metrics_ts 
+    ON raw.metrics (ts DESC);
+
+-- -----------------
+
+CREATE DATABASE analytics;
 
 -- Stores aggregated metrics
 CREATE TABLE IF NOT EXISTS analytics.aggregated_metrics (
@@ -20,13 +25,20 @@ CREATE TABLE IF NOT EXISTS analytics.aggregated_metrics (
     avg_value NUMERIC,
     min_value NUMERIC,
     max_value NUMERIC,
-    p90_value NUMERIC
+    p90_value NUMERIC,
+    p80_value NUMERIC
 );
+CREATE INDEX IF NOT EXISTS idx_aggregated_metrics_ts 
+    ON analytics.aggregated_metrics (ts DESC);
+-- CREATE INDEX IF NOT EXISTS idx_aggregated_metrics_server_ts
+--     ON analytics.aggregated_metrics (server_id, ts DESC);
+-- CREATE INDEX IF NOT EXISTS idx_aggregated_metrics_metric_ts
+--     ON analytics.aggregated_metrics (metric_type, ts DESC);
 
 CREATE TABLE IF NOT EXISTS analytics.run_state (
     id BOOLEAN PRIMARY KEY DEFAULT TRUE,
     last_processed_ts TIMESTAMPTZ
 );
-INSERT INTO analytics.aggregation_run_state (last_processed_ts, interval_minutes)
-VALUES ('1970-01-01', 5);
+INSERT INTO analytics.run_state (id, last_processed_ts)
+VALUES (TRUE, '1970-01-01');
 
